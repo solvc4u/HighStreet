c1a079044ffae44145a09a67ba91c0a6b0dd7f95
solvc4u <solvc4u@gmail.com>
2025-04-29 14:50:30 +0100

    feact: create

---
+++ Bugs&Features.md
@@ -0,0 +1,27 @@
+## Frontend
+### Features to Add
+- Twitter sharing embeded for when User Buys, and Sells. Every Buy shares on twitter link to product, every sell shares how much money user made. 
+- Metamask to set State on buy buttons and linked with "connect Wallet" button both in Nav and in Trade Card 
+- Disable Sell and Redeem button when user does not have token in wallet 
+- Pineterest like design for cards on Discovery Page, or better design 
+- Discovery page cards to link to Trade page landing on the same item clicked on, also do the same for Trade Button on Home page 
+- Above point clarification (prop.name === p.name inside the map) 
+- Merchant page for sign up and set up shop 
+
+
+### Bugs
+- For Brands Button turns opaque white upon hover, need better design 
+- Trade page sliding cards need to be worked out better 
+
+
+## Backend
+### Features to Add
+- Done
+
+### Bugs
+
+## Contract 
+- Pricing Function implementation, decide whether to pre-mine to set price or to use new function derived 
+- simplify K implementation 
+- Smart Contract Managing platform token 
+- Token Vesting Automation 
---
+++ truffle-config.js
@@ -0,0 +1,160 @@
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * trufflesuite.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura accounts
+ * are available for free at: infura.io/register.
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
+const wrapProvider = require('arb-ethers-web3-bridge').wrapProvider;
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const infuraKey = "fj4jll3k.....";
+//
+const fs = require('fs');
+// For Kovan testnet and mainnet using Infura
+let privateKeyTest ="";
+let privateKey ="";
+if (fs.existsSync(".secretTest")) {
+  privateKeyTest = fs.readFileSync(".secretTest").toString().trim();
+}
+if (fs.existsSync(".secret")) {
+  privateKey = fs.readFileSync(".secret").toString().trim();
+} else if(privateKeyTest !== "") {
+  privateKey = privateKeyTest;
+}
+
+const mainnetEndpointUrl = fs.readFileSync(".mainnetEndpoint").toString().trim();
+const kovanEndpointUrl = fs.readFileSync(".kovanEndpoint").toString().trim();
+const rinkebyEndpointUrl = fs.readFileSync(".rinkebyEndpoint").toString().trim();
+
+// Using Arbitrum chain on Kovan net.
+// const mnemonic = fs.readFileSync(".mnemonic").toString().trim();
+
+module.exports = {
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
+  networks: {
+    // Useful for testing. The `development` name is special - truffle uses it by default
+    // if it's defined here and no other network is specified at the command line.
+    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // options below to some value.
+    //
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
+
+    // Another network with more advanced options...
+    // advanced: {
+    // port: 8777,             // Custom port
+    // network_id: 1342,       // Custom network
+    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    // from: <address>,        // Account to send txs from (default: accounts[0])
+    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
+    // },
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    mainnet: {
+      provider: () => new HDWalletProvider([privateKey], mainnetEndpointUrl),
+      network_id: 1,
+      gas: 30000000,
+      gasPrice: 10000000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      websocket: true,
+      timeoutBlocks: 50000,
+      networkCheckTimeout: 10000000
+    },
+    kovan: {
+      provider: () => new HDWalletProvider([privateKeyTest], kovanEndpointUrl),
+      network_id: 42,
+      gas: 12487794,
+      gasPrice: 10000000000,
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider([privateKeyTest], rinkebyEndpointUrl),
+      network_id: 4,
+      gas: 20000000,
+      gasPrice: 10000000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      websocket: true,
+      timeoutBlocks: 50000,
+      networkCheckTimeout: 10000000
+    },
+    arbitrum: {
+      provider: function () {
+        return wrapProvider(
+          new HDWalletProvider(mnemonic, 'https://rinkeby.arbitrum.io/rpc')
+        )
+      },
+      network_id: '*', // Match any network id
+      gas: 450000000,
+      gasPrice: 0,
+    },
+
+    // Useful for private networks
+    // private: {
+    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+    // network_id: 2111,   // This network is yours, in the cloud.
+    // production: true    // Treats this network as if it was a public net. (default: false)
+    // }
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+  contracts_directory: './client/src/contracts/',
+  contracts_build_directory: './client/src/build/contracts',
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.3",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 50
+       },
+       evmVersion: "byzantium"
+      }
+    }
+  },
+
+  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
+  //
+  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
+  // those previously migrated contracts available in the .db directory, you will need to run the following:
+  // $ truffle migrate --reset --compile-all
+
+  db: {
+    enabled: false
+  }
+};
